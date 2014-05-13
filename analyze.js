6adeb76ab1b883da9f25efcb1b7c9785d068b189
janecakemaster <janecakemaster@gmail.com>
2014-05-13 03:56:46 -0400

    walks tree, looks for 'require' (no analysis yet)

---
+++ analyze.js
@@ -0,0 +1,47 @@
+/*
+* analyze.js :: walk the AST
+*
+*/
+
+var fs = require('fs'),
+    async = require('async');
+
+var analyze = module.exports = function analyze(ast, callback) {
+  var stats = {}; //1
+  stats.counter = 0;
+
+  walk(ast, function(node) {
+    if (node.name === 'require') {
+      console.log("something was required");
+      stats.counter++;
+    } else {
+      // console.log("someting else");
+    }
+  });
+  processResults(stats);
+  callback(null, ast);
+}
+
+// recursive func for traversing tree
+function walk(node, func) {
+  func(node);
+  for (var key in node) {
+    if (node.hasOwnProperty(key)) {
+      var child = node[key];
+      if (typeof child === 'object' && child !== null) {
+        if (Array.isArray(child)) {
+          child.forEach(function(node) {
+            walk(node, func);
+          });
+        } else {
+          walk(child, func);
+        }
+      }
+    }
+  }
+}
+
+function processResults(results) {
+  console.log(results);
+}
+
--- test/simple-test.js
+++ test/simple-test.js
@@ -1,16 +1,21 @@
 var test = require('tap').test,
     fs = require('fs'),
     path = require('path'),
-    pipeline = require('../pipeline');
+    pipeline = require('../pipeline'),
+    analyze = require('../analyze');
 
 test('Do we work', function (t) {
-  t.plan(2);
+  t.plan(3);
   pipeline('winston', function (err, ast) {
     if (err) return t.fail(err.message);
     t.ok(ast, 'Resolved an AST object for winston');
     fs.writeFile(path.join(__dirname, 'winston.json'), JSON.stringify(ast, null, 2), function (err) {
       if (err) return t.fail(err.message);
       t.ok(true, 'Successfully written file of winston');
+    });
+    analyze(ast, function (err) {
+      if (err) return t.fail(err.message);
+      t.ok(true, 'Walked the tree for winston');
     });
   });
 });
