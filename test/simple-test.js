173acef71af8b95d078326c59ec1616bdb7a255e
Jarrett Cruger <jcrugzz@gmail.com>
2014-05-12 23:42:32 -0400

    [test] have the test case write a file of the resolution

--- test/simple-test.js
+++ test/simple-test.js
@@ -1,10 +1,16 @@
 var test = require('tap').test,
+    fs = require('fs'),
+    path = require('path'),
     pipeline = require('../pipeline');
 
 test('Do we work', function (t) {
-  t.plan(1);
-  pipeline('winston', function (err) {
+  t.plan(2);
+  pipeline('winston', function (err, ast) {
     if (err) return t.fail(err.message);
-    t.ok(!err, 'no error');
+    t.ok(ast, 'Resolved an AST object for winston');
+    fs.writeFile(path.join(__dirname, 'winston.json'), JSON.stringify(ast, null, 2), function (err) {
+      if (err) return t.fail(err.message);
+      t.ok(true, 'Successfully written file of winston');
+    });
   });
 });
